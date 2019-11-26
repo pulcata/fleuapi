65167d1c07b28c1af9ef08c4e73f7d50bf8bc8c9
Angel Ramirez <angel.ramirez@wizeline.com>
2019-11-25 22:52:36 -0600

    Add elements to deploy

--- functions/index.js
+++ functions/index.js
@@ -6,3 +6,22 @@ const functions = require('firebase-functions');
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+import express from 'express';
+import { urlencoded, json } from 'body-parser';
+
+var app = express();
+app.use(urlencoded( { extended : false } ));
+app.use(json());
+
+import userRoutes from './routes/user';
+import generalRoutes from './routes/general';
+import plantRoutes from './routes/plant';
+import placeRoutes from './routes/place';
+
+app.use('/api', generalRoutes);
+app.use('/api/user', userRoutes);
+app.use('/api/plant', plantRoutes);
+app.use('/api/place', placeRoutes)
+
+export default app;
