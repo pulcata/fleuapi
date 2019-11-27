6866e113cc904199dde966167d34ee7e2f37c398
Chipotle coders <savir@chipotie.io>
2019-11-26 19:38:43 -0600

    Add default route

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const express = require('express')
 const bodyParser =  require('body-parser')
 
 var app = express();
-app.use(bodyParser.urlencoded( { extended : false } ));
+app.use(bodyParser.urlencoded( { extended : false } ))
 app.use(bodyParser.json());
 
 const userRoutes = require ('./routes/user')
@@ -12,9 +12,10 @@ const generalRoutes = require('./routes/general')
 const plantRoutes = require('./routes/plant')
 const placeRoutes = require('./routes/place')
 
-app.use('/api', generalRoutes);
-app.use('/api/user', userRoutes);
-app.use('/api/plant', plantRoutes);
+app.use('/api', generalRoutes)
+app.use('/', generalRoutes)
+app.use('/api/user', userRoutes)
+app.use('/api/plant', plantRoutes)
 app.use('/api/place', placeRoutes)
 
 module.exports = app
